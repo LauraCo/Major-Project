8138d9494c3902710f0b0fd4a920306a70ced673
LauraCo <lac32@aber.ac.uk>
2016-02-05 11:16:53 +0000

    Open and close file dialog

---
+++ Source/Spike/testGUI.m
@@ -0,0 +1,87 @@
+function varargout = testGUI(varargin)
+% TESTGUI MATLAB code for testGUI.fig
+%      TESTGUI, by itself, creates a new TESTGUI or raises the existing
+%      singleton*.
+%
+%      H = TESTGUI returns the handle to a new TESTGUI or the handle to
+%      the existing singleton*.
+%
+%      TESTGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TESTGUI.M with the given input arguments.
+%
+%      TESTGUI('Property','Value',...) creates a new TESTGUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before testGUI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to testGUI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help testGUI
+
+% Last Modified by GUIDE v2.5 05-Feb-2016 11:14:08
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @testGUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @testGUI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before testGUI is made visible.
+function testGUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to testGUI (see VARARGIN)
+
+% Choose default command line output for testGUI
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes testGUI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = testGUI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+[filename, pathname] = uigetfile('*.m', 'Select a MATLAB code file');
+if isequal(filename,0)
+   disp('User selected Cancel')
+else
+   disp(['User selected ', fullfile(pathname, filename)])
+end
