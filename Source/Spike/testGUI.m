69e9b5614a97e2b7660fa1779b7cc31e90dafc9d
LauraCo <lac32@aber.ac.uk>
2016-02-06 11:53:48 +0000

    New buttons in GUI

--- Source/Spike/testGUI.m
+++ Source/Spike/testGUI.m
@@ -22,7 +22,7 @@ function varargout = testGUI(varargin)
 
 % Edit the above text to modify the response to help testGUI
 
-% Last Modified by GUIDE v2.5 05-Feb-2016 11:27:09
+% Last Modified by GUIDE v2.5 05-Feb-2016 16:10:47
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -79,18 +79,18 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-%[filename, pathname] = uigetfile('*.jpg', 'Select an image file');
-%if isequal(filename,0)
-%   disp('User selected Cancel')
-%else
-%   disp(['User selected ', fullfile(pathname, filename)])
-%end
+[filename, pathname] = uigetfile('*.jpeg', 'Select an image file');
+if isequal(filename,0)
+   disp('User selected Cancel')
+else
+   disp(['User selected ', fullfile(pathname, filename)])
+   img = imread(filename);
+   axes(handles.axes1);
+   imshow(img);
+end
 
-img = imread('me.jpeg');
+
 %image(img)
-axes(handles.axes1);
-imshow(img);
-
 
 
 % --- Executes on button press in pushbutton2.
@@ -104,4 +104,27 @@ if isequal(filename,0) || isequal(pathname,0)
    disp('User selected Cancel')
 else
    disp(['User selected ',fullfile(pathname,filename)])
+end
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
 end
