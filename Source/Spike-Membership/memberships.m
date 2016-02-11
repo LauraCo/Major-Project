dc4a2d22f8b03a772e4fa432f67851722987a6c6
LauraCo <lac32@aber.ac.uk>
2016-02-11 16:37:20 +0000

    Add histogram work to grayscale image

--- Source/Spike-Membership/memberships.m
+++ Source/Spike-Membership/memberships.m
@@ -4,15 +4,19 @@ function [ output_args ] = memberships( input_args )
 
 Irgb = imread('me.jpeg');
 
-%tandard NTSC conversion formula
+%Standard NTSC conversion formula
 IGrey = 0.2989*Irgb(:,:,1)+0.5870*Irgb(:,:,2)+0.1140*Irgb(:,:,3);
 
 BW = im2bw(IGrey, 0.4);
 
 %Plot the images side by side
-subplot(1,3,1), subimage(Irgb)
-subplot(1,3,2), subimage(IGrey)
-subplot(1,3,3), subimage(BW)
+%http://www.mathworks.com/matlabcentral/answers/101806-how-can-i-insert-my-matlab-figure-fig-files-into-multiple-subplots
+subplot(1,3,1), subimage(Irgb),title('Original Image')
+subplot(1,3,2), subimage(IGrey), title('Greyscale Image')
+subplot(1,3,3), subimage(BW), title('Black & White Image')
 
+figure;
+subplot(1,2,2), subimage(IGrey), title('Greyscale Image')
+subplot(1,2,1), imhist(IGrey) %http://uk.mathworks.com/help/images/ref/imhist.html
 
 
