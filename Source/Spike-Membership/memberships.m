183dd5888e49338f26bdc342d258071c665f1b17
LauraCo <lac32@aber.ac.uk>
2016-02-11 16:28:46 +0000

    Playing around with image conversions

--- Source/Spike-Membership/memberships.m
+++ Source/Spike-Membership/memberships.m
@@ -9,6 +9,7 @@ IGrey = 0.2989*Irgb(:,:,1)+0.5870*Irgb(:,:,2)+0.1140*Irgb(:,:,3);
 
 BW = im2bw(IGrey, 0.4);
 
+%Plot the images side by side
 subplot(1,3,1), subimage(Irgb)
 subplot(1,3,2), subimage(IGrey)
 subplot(1,3,3), subimage(BW)
