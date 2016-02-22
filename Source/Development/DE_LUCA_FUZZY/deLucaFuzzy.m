241cdd2d456539ff7309e04e81f58c3efba58e99
LauraCo <lac32@aber.ac.uk>
2016-02-22 14:15:29 +0000

    Update De-Luca
    Update path to membership (needs full for some reason?), update enTable
    equation

--- Source/Development/DE_LUCA_FUZZY/deLucaFuzzy.m
+++ Source/Development/DE_LUCA_FUZZY/deLucaFuzzy.m
@@ -2,22 +2,24 @@ function entropy = deLucaFuzzy(meanImg)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-addpath ('../MEMBERSHIP')
+addpath /Users/lauracollins/Git/Major-Project/Source/Development/MEMBERSHIP 
 
 imgMu = membership(meanImg);
 
 
-disp(imgMu);
+%disp(imgMu);
 
 
 persistent entTable;
 if size(entTable)==0
   fprintf(1,'Computing entropy lookup table...');
-  entTable= -(imgMu.*log(imgMu)+(1-imgMu).*log(1-imgMu));
+  entTable= imgMu.*log(imgMu)+(1-imgMu).*log(1-imgMu);
   fprintf(1,'done.\n');
 end
 
-ent= sum(sum(entTable(floor(meanImg*9999.999999+1)))); %LAC32 - what is this line doing?!
+%WORKS UP TO HERE
+
+ent= (sum(sum(entTable(floor(meanImg*9999.999999+1))))); %LAC32 - -K x the sum of the ent, which has been normalised
 entropy=ent/numel(meanImg);   % Return mean pixel entropy. %LAC32 - NUMEL is usually faster than PROD
 
 end
