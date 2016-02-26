35f5e90adf3b5e165c71ebb38e8ba499c6569ef0
LauraCo <lac32@aber.ac.uk>
2016-02-26 16:50:26 +0000

    Tidy up De Luca function

--- Source/Development/DE_LUCA_FUZZY/deLucaFuzzy.m
+++ Source/Development/DE_LUCA_FUZZY/deLucaFuzzy.m
@@ -5,25 +5,6 @@ addpath /Users/lauracollins/Git/Major-Project/Source/Development/MEMBERSHIP
 
 imgMu = membership(meanImg);
 
-%dlmwrite('imgMu.txt',imgMu);
-
-%imageEnt = [];
-
-%for i=1:size(imgMu)
-    
-   % if imgMu{i}==1
-  %      pixelEnt = 0;
- %   else
-
- %   pixelEnt = (imgMu{i}.*log10(imgMu{i})+(1-imgMu{i}).*log10(1-imgMu{i}));
-%    end
- %   imageEnt{i} = pixelEnt;
-   
-%end
-
-
-%dlmwrite('imageEnt.txt',imageEnt);
-
 persistent entTable;
 if size(entTable)==0
   fprintf(1,'Computing entropy lookup table...');  
@@ -36,11 +17,6 @@ if size(entTable)==0
   end
   fprintf(1,'done.\n');
 end
-
-%dlmwrite('entTable.txt',entTable);
-
-
-%WORKS UP TO HERE
 
 %ent= (sum(sum(entTable(floor(meanImg*9999.999999+1))))); %LAC32 - -K x the sum of the ent, which has been normalised
 
