96eefcf19644b0025d3b307b1470e7bf2da21ed5
LauraCo <lac32@aber.ac.uk>
2016-03-07 17:02:25 +0000

    Edit membership test

--- Source/Development/MEMBERSHIP/membershipTest.m
+++ Source/Development/MEMBERSHIP/membershipTest.m
@@ -2,12 +2,13 @@ function tests = membershipTest
 % Test the Membership function
 % http://uk.mathworks.com/help/matlab/matlab_prog/write-function-based-unit-tests-.html
 
+
 tests = functiontests(localfunctions);
 
 end
 
-function testMembershipDegree(testCase)
-    actMemDegree = membership('test.jpeg',80,230); % Pass in the image, and the coordinate to check membership degree of
-    expMemDegree = 0.644444444444444; % From previous SPIKE work, this is the known membership degree
-    testCase.verifyEqual(actMemDegree,double(expMemDegree), 'RelTol', 0.0001);
+function testMembershipDegree(testCase)  
+    testImage = imread('../test.jpeg');
+    memDegree = membership(testImage); 
+    verifyNotEmpty(testCase, memDegree);
 end
