84df9bba3a62fc4324d5803df0a8c10623bb03bd
LauraCo <lac32@aber.ac.uk>
2016-03-11 12:19:44 +0000

    Create large pgm of mammograms
    Some error with rotation and mirroring of images when writing out to
    larger file - investigate this further.

---
+++ Source/Development/IO/pgm2bigPgm.m
@@ -0,0 +1,27 @@
+%    filename = strcat('/Users/lauracollins/Git/Major-Project/Source/Development/all-mias/sample/',scanDirectory(i).name);
+%    I = imread(filename);
+%    figure, imshow(I);
+
+scanDirectory = dir('/Users/lauracollins/Git/Major-Project/Source/Development/all-mias/sample/*.pgm');
+noOfScans = length(scanDirectory);
+
+filename = strcat('/Users/lauracollins/Git/Major-Project/Source/Development/all-mias/sample/',scanDirectory(1).name);
+file=fopen(filename,'r');
+
+ln1=fgetl(file);
+ln2=strsplit(fgetl(file));
+squareImageSize = str2double(ln2);
+
+
+sers=zeros(squareImageSize(1),squareImageSize(2),noOfScans);
+
+for i = 1:noOfScans
+    scan = fopen(strcat('/Users/lauracollins/Git/Major-Project/Source/Development/all-mias/sample/',scanDirectory(i).name));
+    im=(fread(scan,[squareImageSize(1),squareImageSize(2)],'uchar'));
+    sers(:,:,i) = im;
+end
+
+
+outfname=sprintf('/Users/lauracollins/Git/Major-Project/Source/Development/all-mias/sample/big_scan.pgm');
+s=sers(:,:,:);
+saveSeries(outfname,s);
