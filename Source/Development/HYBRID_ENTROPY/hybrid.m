6a1f6223a3761e3f73913787cf303b6a47967228
LauraCo <lac32@aber.ac.uk>
2016-03-24 14:12:26 +0000

    Initial hybrid function

---
+++ Source/Development/HYBRID_ENTROPY/hybrid.m
@@ -0,0 +1,10 @@
+function entropy = hybrid(meanImg)
+%HYBRID Summary of this function goes here
+%   Detailed explanation goes here
+
+addpath /Users/lauracollins/Git/Major-Project/Source/Development/MEMBERSHIP 
+
+[imgMu, lowMu, medMu, highMu] = membership(meanImg);
+
+end
+
