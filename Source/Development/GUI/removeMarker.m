cac8b7b7349b3c095d48037f13995a0e2bc8f65f
LauraCo <lac32@aber.ac.uk>
2016-03-28 18:04:54 +0100

    Save pgm file now working
    Congealing algorithm needs pgm file format to be exact, with new lines
    in the header, rather than the usually fine space.

--- Source/Development/GUI/removeMarker.m
+++ Source/Development/GUI/removeMarker.m
@@ -132,4 +132,15 @@ function pushbutton2_Callback(hObject, eventdata, handles)
 %http://uk.mathworks.com/help/matlab/ref/uiputfile.html
 
 [file,path] = uiputfile(strcat(handles.imagePath,handles.imageName),'Save file name');
-imwrite(handles.finalImg, strcat(path,file), 'pgm');
+
+[imHeight,imWidth, dim] = size(handles.finalImg);
+
+output = fopen(strcat(path,file), 'w');
+
+fprintf(output,'P5\n');
+fprintf(output,'%d %d\n',imWidth,imHeight);
+fprintf(output,'255\n');
+fwrite(output,handles.finalImg,'uchar');
+fclose(output);
+
+%imwrite(handles.finalImg, strcat(path,file), 'pgm');
