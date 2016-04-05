01e6a8311a382ddc41be9f03a2536320113fc961
LauraCo <lac32@aber.ac.uk>
2016-04-05 11:37:07 +0100

    Fix the hybrid issue

--- Source/Development/HYBRID_ENTROPY/hybrid.m
+++ Source/Development/HYBRID_ENTROPY/hybrid.m
@@ -23,19 +23,20 @@ end
 
 %Trapezium A - Low grey-level values
 
-eLowMed0 = ((1 - medMu) - lowMu).*exp(lowMu); % Likelihood of receiving medium 
-eLowMed0 = sum(1/numel(lowMu)*sum(eLowMed0));
+eLowMed0 = (1 - medMu).*exp(lowMu); % Likelihood of receiving medium 
+eLowMed0 = sum(1/numel(lowMu+medMu)*sum(eLowMed0));
 
-eLowMed1 = lowMu.*exp((1 - medMu) - lowMu);
-eLowMed1 = sum(1/numel(lowMu)*sum(eLowMed1));
+eLowMed1 = (1 - lowMu).*exp(medMu); % Likelihood of receiving medium 
+eLowMed1 = sum(1/numel(lowMu+medMu)*sum(eLowMed1));
 
 %Trapezium B - Medium grey-level values
 
-eMedHigh0 = ((1 - highMu) - medMu).*exp(medMu);
-eMedHigh0 = sum(1/numel(medMu)*sum(eMedHigh0));
+eMedHigh0 = (1 - highMu).*exp(medMu);
+eMedHigh0 = sum(1/numel(medMu+highMu)*sum(eMedHigh0));
 
-eMedHigh1 = medMu.*exp((1 - highMu) - medMu);
-eMedHigh1 = sum(1/numel(medMu)*sum(eMedHigh1));
+eMedHigh1 = (1 - medMu).*exp(highMu);
+eMedHigh1 = sum(1/numel(medMu+highMu)*sum(eMedHigh1));
+
 
 %eLowMed0 = lowMu.*exp((1 - medMu) - lowMu);
 %eLowMed0 = sum(1/numel(lowMu)*sum(eLowMed0));
@@ -60,7 +61,7 @@ eMedHigh1 = sum(1/numel(medMu)*sum(eMedHigh1));
 %hybridMed = -medCount / (lowCount+medCount+highCount).*log10(1 - eMedHigh0) - ((lowCount+highCount) / (lowCount+medCount+highCount).*log10(eMedHigh1)); % 0.243414
 
 hybridLow = -lowCount / (lowCount+medCount).*log10(1 - eLowMed0) - (medCount / (lowCount+medCount).*log10(eLowMed1));
-hybridMed = -medCount / (medCount+highCount).*log10(1 - eMedHigh0) - (highCount / (lowCount+medCount+highCount).*log10(eMedHigh1)); % 0.242960
+hybridMed = -medCount / (medCount+highCount).*log10(1 - eMedHigh0) - (highCount / (medCount+highCount).*log10(eMedHigh1)); % 0.242960
 
 %if isnan(hybridHigh)
 %    hybridHigh = 0;
