b3d51227462bd63bc40f0968c3027c24cc4dd6a6
LauraCo <lac32@aber.ac.uk>
2016-04-07 14:01:47 +0100

    New membership class for Hybrid
    Only 2 trapeziums

--- Source/Development/HYBRID_ENTROPY/hybrid.m
+++ Source/Development/HYBRID_ENTROPY/hybrid.m
@@ -4,19 +4,16 @@ function entropy = hybrid(meanImg)
 
 addpath /Users/lauracollins/Git/Major-Project/Source/Development/MEMBERSHIP 
 
-[imgMu, lowMu, medMu, highMu] = membership(meanImg);
+[imgMu, lowMu, highMu] = hybridMembership(meanImg);
 
 
 lowCount = 0;
-medCount = 0;
 highCount = 0;
 
 for i=1:numel(imgMu)
-    if lowMu(i) > medMu(i) && lowMu(i) > highMu(i)
+    if lowMu(i) > highMu(i)
         lowCount = lowCount + 1;
-    elseif medMu(i) > lowMu(i) && medMu(i) > highMu(i)
-        medCount = medCount + 1;
-    elseif highMu(i) > lowMu(i) && highMu(i) > medMu(i)
+    elseif highMu(i) > lowMu(i)
         highCount = highCount + 1;
     end
 end
@@ -29,46 +26,46 @@ eLow0 = (1/(lowCount)) * sum(sum(eLow0));
 eLow1 = lowMu.*exp(1-lowMu);
 eLow1 = (1/(lowCount)) * sum(sum(eLow1));
 
+
+
 %Trapezium B - Medium grey-level values
 
 
-eMed0 = (1 - medMu).*exp(medMu);
-eMed0 = (1/medCount) * sum(sum(eMed0));
+%eMed0 = (1 - medMu).*exp(medMu);
+%eMed0 = (1/medCount) * sum(sum(eMed0));
 
-eMed1 = medMu.*exp(1-medMu);
-eMed1 = (1/medCount) * sum(sum(eMed1));
+%eMed1 = medMu.*exp(1-medMu);
+%eMed1 = (1/medCount) * sum(sum(eMed1));
 
 %Trapezium C - High grey-level values
 
-eHigh0 = (1 - highMu).*exp(highMu);
-eHigh0 = (1/highCount) * sum(sum(eHigh0));
+%eHigh0 = (1 - highMu).*exp(highMu);
+%eHigh0 = (1/highCount) * sum(sum(eHigh0));
 
-if isnan(eHigh0)
-    eHigh0 = 0;
-end
-
-eHigh1 = highMu.*exp(1-highMu);
-eHigh1 = (1/highCount) * sum(sum(eHigh1));
-
-if isnan(eHigh1)
-    eHigh1 = 0;
-end
+%eHigh1 = highMu.*exp(1-highMu);
+%eHigh1 = (1/highCount) * sum(sum(eHigh1));
 
 
-%If eHigh0/1 = NAN, set to 0...
 
+hybridLow = -(lowCount / (lowCount+highCount))*log10(1 - eLow0);
+hybridLow = hybridLow - (highCount / (lowCount+highCount)*log10(eLow1));
 
-hybridLow = -(lowCount / (lowCount+medCount+highCount))*log10(1 - eLow0);
-hybridLow = hybridLow - (medCount / (lowCount+medCount+highCount)*log10(eLow1));
+%hybridMed = -(medCount / (medCount+highCount))*log10(1 - eMed0);
+%hybridMed = hybridMed - (highCount / (medCount+highCount)*log10(eMed1)); % Do I compare this to low trapezium, high trapezium or both??
 
-hybridMed = -(medCount / (lowCount+medCount+highCount))*log10(1 - eMed0);
-hybridMed = hybridMed - (highCount / (lowCount+medCount+highCount)*log10(eMed1)); % Do I compare this to low trapezium, high trapezium or both??
+%hybridHigh = -(highCount / (lowCount+highCount))*log10(1 - eHigh0);
+%hybridHigh = hybridHigh - (lowCount / (lowCount+highCount)*log10(eHigh1));
 
-hybridHigh = -(highCount / (lowCount+medCount+highCount))*log10(1 - eHigh0);
-hybridHigh = hybridHigh - (medCount / (lowCount+medCount+highCount)*log10(eHigh1));
+%if isnan(hybridHigh)
+%    hybridHigh = 0;
+%end
 
-totalHybrid = hybridLow + hybridMed + hybridHigh;
-entropy = totalHybrid / 3; 
+%fprintf(1,'Hybrid Low %f\n',hybridLow);
+%fprintf(1,'Hybrid Med %f\n',hybridMed);
+%fprintf(1,'Hybrid High %f\n',hybridHigh);
+
+%totalHybrid = hybridLow + hybridHigh;
+entropy = hybridLow; 
 
 end
 
